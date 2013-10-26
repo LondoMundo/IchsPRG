f78eff0e02efa39e69020f54aed6e773cf27ab79
Colin Murphy <dietcoke3.14@gmail.com>
2013-10-26 14:35:54 -0400

    made code more readable

--- rpg.py
+++ rpg.py
@@ -1,11 +1,33 @@
-import os
+try:
+    import time
+    # we import time to use the sleep function. It lets the terminal stay open for a bit after we raise a sysExit
+    x=1
+    # x is = to 1 if time can be imported. This is so that the program will run on sys's w/o time
+except:
+    x=0
+# used to randomize moves that are used
 from random import randint
-global strength
+
+#global strength  implement this as a modifyer to the damage the player deals
+
 def changeArea():
+    """This function is called when a player wants to change areas"""
+
     print "What area do you want to change into? 2 or 3?"
     areaChoice = raw_input()
     return areaChoice
+#Make moves based on expereience, not level. Make exp a running total
+
+
+def expStuff(exp):
+    pass
+
+
+
+
+
 def checkMoves(moveOption):
+    """Checks move that the player wants to use against list of held moves"""
     if moveOption == heldmoves[0]:
         return True
     elif moveOption == heldmoves[1]:
@@ -19,20 +41,45 @@ def checkMoves(moveOption):
 
 def moveDamage(move):
     return allMoves[move]
-    
+
+def moveUsed(currentArea):
+    """Calculates which move the opponent is to use. It is different if they are in different areas, hence the if
+    statement"""
+    if currentArea == 2:
+        moveRandom=randint(0,1)
+        usedMove=oppMoves2[moveRandom]
+        return usedMove
+    elif currentArea == 3:
+        moveRandom=randint(0,1)
+        usedMove=oppMoves3[moveRandom]
+        return usedMove
+
 
 def whatMove():
+    """
+    prints all of the users held moves. Takes input from the user. turns that input lowercase.return lowercase input
+    """
     print "Do you want to " + heldmoves[0] + " , " + heldmoves[1] + " , " + heldmoves[2] + " , " + heldmoves[3]
     moveOption=raw_input("")
     moveOption=moveOption.lower()
     return moveOption
-pHealth=200
-health={'Alex':100, 'Nathan':100, 'Demas':200, 'Pries':225, 'Bosma': 175, 'Laurie':200, 'Jacobsen':150, 'Dipzinski':225, 'Bebee':300, 'Gymory':350, 'Lestage':325, 'Koss':250, 'Gartrell':400, 'Halstead':350}
+
+#initializes the players health
+pHealth=200checkLevel(exp)
+#list the starting health of opponents
+health={'Alex': 100, 'Nathan': 100, 'Demas':200, 'Pries': 225, 'Bosma': 175, 'Laurie': 200, 'Jacobsen': 150, 'Dipzinski'
+: 225, 'Bebee': 300, 'Gymory': 350, 'Lestage': 325, 'Koss': 250, 'Gartrell': 400, 'Halstead':350}
 allMoves = {'punch':50, 'kick':60, 'karate':60}
+oppMoves2=['hit', 'yell']
+oppDamage2={'hit':50, 'yell':60}
+oppMoves3=['hard hit', 'screech']
+oppDamage3={'hard hit':75, 'screech':100}
 heldmoves = ["punch", "kick", "do nothing", "do nothing"]
 wades=["Alex", "Nathan"]
 v2Teach=["Demas", "Pries", "Bosma", "Laurie", "Jacobsen", "Dipzinski"]
 v3Teach=["Bebee", "Gymory", "Lestage", "Koss", "Gartrell", "Halstead"]
+exp=0
+
 print "You arrive at ICHS, a mystical land filled with mosterous Wades, and"
 print "friendly fairys."
 print "you arrive at the front doors of the high school. What do you want to do?"
@@ -50,24 +97,24 @@ print "what do you want to do? [F]ight or [R]un?"
 FightOrRun=raw_input("")
 FightOrRun=FightOrRun.lower()
 if FightOrRun=="f":
-    
+
     oppHealth=health[wade]
     while oppHealth >0 and pHealth >0:
-        
+
         print "he has "+`oppHealth`+" health"
         print "you have "+`pHealth` + "health"
         moveOption = whatMove()
-       
+
         if checkMoves(moveOption) == True:
             print "you " + moveOption + " your oponent"
             oppHealth = oppHealth - 50
             print "wade used cry"
             print "it did 10 damage"
             pHealth=pHealth-10
-            
+
         else:
             print "you dont have that move"
-            
+
 if oppHealth < 1:
     print "you won"
 elif pHealth<0:
@@ -84,16 +131,21 @@ elif newMove == "karate":
     heldmoves[1]="karate"
     print heldmoves
 print "You have passed the first area. You gained 10 exp. You have now moved onto area two of ICHS."
+
 currentArea=2
+
 while 1==1:
+
     print "you are in area "+`currentArea`
     print "What do you want to do? [F]ight, [C]hange area?"
     whatToDo=raw_input("")
     whatToDo=whatToDo.lower()
+
     if whatToDo == "c":
         currentArea=changeArea()
         currentArea=int(currentArea)
         print currentArea
+
     elif whatToDo == "f":
         if currentArea==2:
             opponent = v2Teach[randint(0,5)]
@@ -109,17 +161,40 @@ while 1==1:
                     oppHealth = oppHealth-damage
                 else:
                     print "That isn't a move"
-                
+
+                usedMove=moveUsed(currentArea)
+                print opponent + " used " + usedMove
+                pHealth -= oppDamage2[usedMove]
+                print "you have "+`pHealth` + " health left"
+
             if pHealth <=0:
+
                 print "you lost"
+                if x==1:
+                    time.sleep(10)
+                    print "i'm sleeping"
+                    raise SystemExit
+
+                elif x==0:
+
+                    print "i didnt sleep"
+                    raise SystemExit
+
             elif oppHealth<=0:
                 print "you won"
+                exp+=100
+
+
+
+
 
         elif currentArea==3:
+
             opponent = v3Teach[randint(0,5)]
             print "A wild " + opponent + " appeared"
             oppHealth=health[opponent]
             while pHealth>0 and oppHealth>0:
+
                 print "The opponent has "+`oppHealth` + " health"
                 print "Do you want to " + heldmoves[0] + " , "+ heldmoves[1]+ " , " +heldmoves[2]+ " , " +heldmoves[3] + "?"
                 moveOption=raw_input("")
@@ -127,10 +202,25 @@ while 1==1:
                 if checkMoves(moveOption)==True:
                     damage=moveDamage(moveOption)
                     oppHealth=oppHealth-damage
+
                 else:
                     print "That isn't a move"
-            if pHealth <=0:
+
+                usedMove=moveUsed(currentArea)
+                print opponent + " used " + usedMove
+                pHealth -= oppDamage3[usedMove]
+                print "you have "+`pHealth` + " health left"
+
+            if pHealth <= 0:
+
                 print "you lost"
+                if x == 1:
+                    time.sleep(10)
+                    raise SystemExit
+
+                elif x == 0:
+                    raise SystemExit
+
             elif oppHealth<=0:
                 print "you won"
         else:
